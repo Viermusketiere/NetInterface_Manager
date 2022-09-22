e8fbbfd237314e272c6a6ff5c92821031ed0c531
Viermusketiere-Kraken <vincent.gauda@online.de>
2022-09-22 23:25:47 +0200

    Started to dynamically add adapters to gui

--- ui_form.py
+++ ui_form.py
@@ -11,46 +11,133 @@
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFormLayout, QHBoxLayout, QPushButton,
-    QSizePolicy, QVBoxLayout, QWidget)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QFormLayout, QFrame, QGridLayout,
+    QLabel, QLineEdit, QMainWindow, QMenu,
+    QMenuBar, QScrollArea, QSizePolicy, QSplitter,
+    QToolBar, QVBoxLayout, QWidget)
 
-class Ui_Widget(object):
-    def setupUi(self, Widget):
-        if not Widget.objectName():
-            Widget.setObjectName(u"Widget")
-        Widget.resize(1242, 719)
-        Widget.setMinimumSize(QSize(200, 200))
-        self.pushButton = QPushButton(Widget)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(340, 80, 141, 24))
-        self.widget = QWidget(Widget)
-        self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(120, 210, 951, 351))
-        self.horizontalLayout = QHBoxLayout(self.widget)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.formLayout = QFormLayout()
+class Ui_mainWindow(object):
+    def setupUi(self, mainWindow):
+        if not mainWindow.objectName():
+            mainWindow.setObjectName(u"mainWindow")
+        mainWindow.resize(1101, 748)
+        self.actionImport_Presets = QAction(mainWindow)
+        self.actionImport_Presets.setObjectName(u"actionImport_Presets")
+        self.actionExport_Presets = QAction(mainWindow)
+        self.actionExport_Presets.setObjectName(u"actionExport_Presets")
+        self.actionRefresh = QAction(mainWindow)
+        self.actionRefresh.setObjectName(u"actionRefresh")
+        self.centralWidget = QWidget(mainWindow)
+        self.centralWidget.setObjectName(u"centralWidget")
+        self.gridLayout = QGridLayout(self.centralWidget)
+        self.gridLayout.setSpacing(6)
+        self.gridLayout.setContentsMargins(11, 11, 11, 11)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.splitter = QSplitter(self.centralWidget)
+        self.splitter.setObjectName(u"splitter")
+        self.splitter.setOrientation(Qt.Horizontal)
+        self.verticalLayoutWidget = QWidget(self.splitter)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.layoutPreset = QVBoxLayout(self.verticalLayoutWidget)
+        self.layoutPreset.setSpacing(6)
+        self.layoutPreset.setContentsMargins(11, 11, 11, 11)
+        self.layoutPreset.setObjectName(u"layoutPreset")
+        self.layoutPreset.setContentsMargins(0, 0, 0, 0)
+        self.scrollArea = QScrollArea(self.verticalLayoutWidget)
+        self.scrollArea.setObjectName(u"scrollArea")
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QWidget()
+        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 535, 678))
+        self.frame = QFrame(self.scrollAreaWidgetContents)
+        self.frame.setObjectName(u"frame")
+        self.frame.setGeometry(QRect(110, 100, 235, 70))
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.formLayout = QFormLayout(self.frame)
+        self.formLayout.setSpacing(6)
+        self.formLayout.setContentsMargins(11, 11, 11, 11)
         self.formLayout.setObjectName(u"formLayout")
+        self.label = QLabel(self.frame)
+        self.label.setObjectName(u"label")
 
-        self.horizontalLayout.addLayout(self.formLayout)
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label)
 
-        self.verticalLayout = QVBoxLayout()
-        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.lineEdit = QLineEdit(self.frame)
+        self.lineEdit.setObjectName(u"lineEdit")
 
-        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.lineEdit)
 
+        self.label_2 = QLabel(self.frame)
+        self.label_2.setObjectName(u"label_2")
 
-        self.retranslateUi(Widget)
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label_2)
 
-        QMetaObject.connectSlotsByName(Widget)
+        self.lineEdit_2 = QLineEdit(self.frame)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.lineEdit_2)
+
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.layoutPreset.addWidget(self.scrollArea)
+
+        self.splitter.addWidget(self.verticalLayoutWidget)
+        self.verticalLayoutWidget_2 = QWidget(self.splitter)
+        self.verticalLayoutWidget_2.setObjectName(u"verticalLayoutWidget_2")
+        self.layoutAdapters = QVBoxLayout(self.verticalLayoutWidget_2)
+        self.layoutAdapters.setSpacing(6)
+        self.layoutAdapters.setContentsMargins(11, 11, 11, 11)
+        self.layoutAdapters.setObjectName(u"layoutAdapters")
+        self.layoutAdapters.setContentsMargins(0, 0, 0, 0)
+        self.scrollArea_Adapters = QScrollArea(self.verticalLayoutWidget_2)
+        self.scrollArea_Adapters.setObjectName(u"scrollArea_Adapters")
+        self.scrollArea_Adapters.setWidgetResizable(True)
+        self.scrollAreaWidgetContents_Adapters = QWidget()
+        self.scrollAreaWidgetContents_Adapters.setObjectName(u"scrollAreaWidgetContents_Adapters")
+        self.scrollAreaWidgetContents_Adapters.setGeometry(QRect(0, 0, 535, 678))
+        self.scrollArea_Adapters.setWidget(self.scrollAreaWidgetContents_Adapters)
+
+        self.layoutAdapters.addWidget(self.scrollArea_Adapters)
+
+        self.splitter.addWidget(self.verticalLayoutWidget_2)
+
+        self.gridLayout.addWidget(self.splitter, 0, 0, 1, 1)
+
+        mainWindow.setCentralWidget(self.centralWidget)
+        self.menuBar = QMenuBar(mainWindow)
+        self.menuBar.setObjectName(u"menuBar")
+        self.menuBar.setGeometry(QRect(0, 0, 1101, 22))
+        self.menuMen = QMenu(self.menuBar)
+        self.menuMen.setObjectName(u"menuMen")
+        mainWindow.setMenuBar(self.menuBar)
+        self.mainToolBar = QToolBar(mainWindow)
+        self.mainToolBar.setObjectName(u"mainToolBar")
+        self.mainToolBar.setLayoutDirection(Qt.RightToLeft)
+        mainWindow.addToolBar(Qt.TopToolBarArea, self.mainToolBar)
+
+        self.menuBar.addAction(self.menuMen.menuAction())
+        self.menuMen.addAction(self.actionImport_Presets)
+        self.menuMen.addAction(self.actionExport_Presets)
+        self.mainToolBar.addAction(self.actionRefresh)
+
+        self.retranslateUi(mainWindow)
+
+        QMetaObject.connectSlotsByName(mainWindow)
     # setupUi
 
-    def retranslateUi(self, Widget):
-        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Network Interface Manager", None))
-        self.pushButton.setText(QCoreApplication.translate("Widget", u"List Interfaces", None))
+    def retranslateUi(self, mainWindow):
+        mainWindow.setWindowTitle(QCoreApplication.translate("mainWindow", u"Network Interface Manager", None))
+        self.actionImport_Presets.setText(QCoreApplication.translate("mainWindow", u"Import Presets", None))
+        self.actionExport_Presets.setText(QCoreApplication.translate("mainWindow", u"Export Presets", None))
+        self.actionRefresh.setText(QCoreApplication.translate("mainWindow", u"Refresh", None))
+        self.label.setText(QCoreApplication.translate("mainWindow", u"IP", None))
+        self.label_2.setText(QCoreApplication.translate("mainWindow", u"Subnetzmaske", None))
+        self.menuMen.setTitle(QCoreApplication.translate("mainWindow", u"Presets", None))
     # retranslateUi
 
--- widget.py
+++ widget.py
@@ -1,31 +1,50 @@
 # This Python file uses the following encoding: utf-8
+from cProfile import label
 import sys
 import networkWin as nw
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QFormLayout, QWidget, QFrame
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py, or
 #     pyside2-uic form.ui -o ui_form.py
-from ui_form import Ui_Widget
+from ui_form import Ui_mainWindow
 
-class Widget(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.ui = Ui_Widget()
+        self.ui = Ui_mainWindow()
         self.ui.setupUi(self)
 
-        self.ui.pushButton.clicked.connect(self.printIf)
+        # self.ui.pushButton.clicked.connect(self.printIf)
+        self.ui.actionRefresh.triggered.connect(self.printIf)
 
-    def printIf(abc):
+    def printIf(parent):
         interfaces = nw.getAdapterInfo()
+
+        frame = QFrame(parent.ui.scrollAreaWidgetContents_Adapters)
+        frame.show()
+        formLayout1 = QFormLayout(frame)
+        
+        labelIP1 = QLabel()
+        labelIP1.setText('IP')
+        labelIP1.show()
+        labelSM1 = QLabel()
+        labelSM1.setText('Subnetzmaske')
+        labelSM1.show()
+        formLayout1.addWidget(labelIP1)
+        formLayout1.addWidget(labelSM1)
+
+        # parent.ui.scrollAreaWidgetContents_Adapters.setWidget(formLayout1)
+
+
         print("Done")
         
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    widget = Widget()
-    widget.show()
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec())
 
